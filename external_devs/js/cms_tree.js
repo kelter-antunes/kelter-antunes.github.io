5aacc96236d45b65586479226e7b50e8c4dfd0a5
kelter <miguel.aka.kelter@gmail.com>
2015-11-17 17:36:17 +0000

    external devs

---
+++ external_devs/js/cms_tree.js
@@ -0,0 +1,109 @@
+var SessionUser = "+ Session.UserId +";
+$(function() {
+    $('#" + ajax.Id + "').jstree({
+        'core': {
+            'themes': {
+                'name': 'proton',
+                'url': '/CMS/jsTree/themes/proton/style.min.css',
+                'responsive': true
+            },
+            'check_callback': function(operation, node, node_parent, node_position, more) {
+                // operation can be 'create_node', 'rename_node', 'delete_node', 'move_node' or 'copy_node'
+                // in case of 'rename_node' node_position is filled with the new node name
+                // return operation === 'rename_node' ? true : false;
+
+                if (operation === 'create_node') {
+
+                    console.log('#### create_node  start #####');
+
+                    console.log(node);
+                    console.log(node_parent);
+                    console.log(node_position);
+                    console.log(more);
+
+                    
+
+                    $.ajax({
+                            url: '//dp1dev.outsystems.com/CMSCore/rest/cms_private/folderOperation',
+                            type: 'GET',
+                            data: {
+                                'operation': operation,
+                                //'node': node.id,
+                                'node_parent': node_parent.id,
+                                'node_position': node_position,
+                                'userid': SessionUser
+
+                            },
+                        })
+                        .done(function(event) {
+
+                            console.log('success');
+                            node.id = event;
+                        })
+                        .fail(function() {
+                            console.log('error');
+                        })
+                        .always(function() {
+                            console.log('complete');
+                        });
+
+
+
+                    console.log('#### create_node  end #####');
+
+                } else if (operation === 'rename_node') {
+
+                    console.log('#### rename_node  start #####');
+                    console.log(node);
+                    console.log(node_parent);
+                    console.log(node_position);
+                    console.log(more);
+
+                    console.log('#### rename_node  end #####');
+
+
+                } else if (operation === 'move_node') {
+                    $.ajax({
+                            url: '//dp1dev.outsystems.com/CMSCore/rest/cms_private/folderOperation',
+                            type: 'GET',
+                            data: {
+                                'operation': operation,
+                                'node': node.id,
+                                'node_parent': node_parent.id,
+                                'node_position': node_position,
+                                'userid': SessionUser
+
+                            },
+                        })
+                        .done(function() {
+                            console.log('success');
+                        })
+                        .fail(function() {
+                            console.log('error');
+                        })
+                        .always(function() {
+                            console.log('complete');
+                        });
+                }
+
+
+
+
+                return true;
+            },
+            'data': {
+                'url': '//dp1dev.outsystems.com/CMSCore/rest/cms_private/getFoldersTree?lazy',
+                'data': function(node) {
+                    externalNode = node;
+                    return {
+                        'id': node.id
+                    };
+                }
+            }
+        },
+        'dnd': {
+            check_while_dragging: false
+        },
+        'plugins': ['contextmenu', 'dnd', 'unique']
+    });
+});
